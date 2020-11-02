7f4ef1595d9c0000503beb77df33177cd32ae420
Abhishek Gautam <abhishekgautm243@gmail.com>
2020-11-02 09:54:44 +0530

    Create HeroSection.js

---
+++ src/components/HeroSection.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const HeroSection = () => {
+  return (
+    <div className="mx-auto max-w-screen-md grid grid-cols-2">HeroSection</div>
+  )
+}
+
+export default HeroSection
