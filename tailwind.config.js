69c26d35805480e03b8c256d88990e80531eda7c
Abhishek Gautam <abhishekgautm243@gmail.com>
2020-11-03 09:26:42 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        80: "20rem",
+        96: "24rem",
+        herosection: "44rem",
+      },
+    },
   },
   variants: {},
   plugins: [],
