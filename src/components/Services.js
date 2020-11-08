121bcbf2df22cfd01fae0afa0a61cb2b049090c5
Abhishek Gautam <abhishekgautm243@gmail.com>
2020-11-08 10:20:43 +0530

    Update Services.js

--- src/components/Services.js
+++ src/components/Services.js
@@ -8,7 +8,7 @@ const Services = () => {
       <div className="max-w-screen-lg xl:pl-4 xl:pr-4 xl:mx-auto mx-5 md:mx-0">
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-y-10 md:gap-y-16 md:gap-x-5 lg:gap-x-10 xl:gap-x-6 mx-10 md:mx-16 lg:mx-24 xl:mx-0">
           <div className="text-center font-lato flex flex-col items-center">
-            <div className="bg-gray-200 rounded-full flex justify-center items-center w-16 h-16">
+            <div className="border shadow-md rounded-full flex justify-center items-center w-16 h-16">
               <MdAttachMoney className="text-xl" />
             </div>
             <h1 className="pt-6 pb-2 font-semibold">Money Back Guarantee</h1>
@@ -17,7 +17,7 @@ const Services = () => {
             </p>
           </div>
           <div className="text-center font-lato flex flex-col items-center">
-            <div className="bg-gray-200 rounded-full flex justify-center items-center w-16 h-16">
+            <div className="border shadow-md rounded-full flex justify-center items-center w-16 h-16">
               <FaTruck className="text-xl" />
             </div>
             <h1 className="pt-6 pb-2 font-semibold">Free Shipping Worldwide</h1>
@@ -26,7 +26,7 @@ const Services = () => {
             </p>
           </div>
           <div className="text-center font-lato flex flex-col items-center">
-            <div className="bg-gray-200 rounded-full flex justify-center items-center w-16 h-16">
+            <div className="border shadow-md rounded-full flex justify-center items-center w-16 h-16">
               <FaLock className="text-xl" />
             </div>
             <h1 className="pt-6 pb-2 font-semibold">Secure Payment</h1>
@@ -35,7 +35,7 @@ const Services = () => {
             </p>
           </div>
           <div className="text-center font-lato flex flex-col items-center">
-            <div className="bg-gray-200 rounded-full flex justify-center items-center w-16 h-16">
+            <div className="border shadow-md rounded-full flex justify-center items-center w-16 h-16">
               <FaPhoneAlt className="text-xl" />
             </div>
             <h1 className="pt-6 pb-2 font-semibold">24/7 Customer Service</h1>
