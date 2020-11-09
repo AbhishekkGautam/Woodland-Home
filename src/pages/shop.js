ac2033588b37b48efc4d4ee5431825a8519fc638
Abhishek Gautam <abhishekgautm243@gmail.com>
2020-11-09 10:25:29 +0530

    Update shop.js

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -8,9 +8,13 @@ const shop = ({ data }) => {
   return (
     <Layout>
       <SEO title="Home" />
-      <div className="max-w-screen-lg xl:pl-4 xl:pr-4 xl:mx-auto mx-12 md:mx-20 h-32 md:h-48 text-3xl md:text-x4l lg:text-5xl text-gray-700 font-semibold tracking-wide bg-purple-100 flex justify-center items-center">
-        <h1 className="font-poppins">Our Products</h1>
-      </div>
+      <section className="w-full">
+        <div className="max-w-screen-lg xl:pl-4 xl:pr-4 xl:mx-auto">
+          <div className="h-40 md:h-48 text-3xl md:text-4xl lg:text-5xl text-gray-700 font-semibold tracking-wide bg-orange-100 flex justify-center items-center">
+            <h1 className="font-light">Our Products</h1>
+          </div>
+        </div>
+      </section>
       <ShopProduct items={data.shop_product} />
     </Layout>
   )
